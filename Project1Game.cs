ef223b8c887276bebeb65df11e1b1f803c783ce3
James McMahon <whichoneiwonder@gmail.com>
2014-10-14 09:54:46 +1100

    added physics library to resourrces, not yet using it properly

--- Project1Game.cs
+++ Project1Game.cs
@@ -18,10 +18,10 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+using System;
+
 using SharpDX;
 using SharpDX.Toolkit;
-using System;
-using System.Collections.Generic;
 
 namespace Project1
 {
@@ -29,14 +29,35 @@ namespace Project1
     // namespace will override the Direct3D11.
     using SharpDX.Toolkit.Graphics;
     using SharpDX.Toolkit.Input;
+    using BEPUphysics;
 
     public class Project1Game : Game
     {
+
+
+        // BEWARE: THE NUMBER OF BINARY FILES SERIALISED TO DISK = 4 TO THE POWER OF tSizeFactor-cSizeFactor. BE CAREFUL!
+        const int   tSizeFactor  = 11,
+                    cSizeFactor  = 6,
+                    loadGridSize = 3;
+        const float tRangeFactor = 0.9f;  // Only change to calibrate overall landscape height. Set between 0.1 and 2.
+
+        private int chunkWidth = (int)Math.Pow(2, cSizeFactor)+1;
+        private int[] lastPlayerZone = {1, 1}; // This is the zone in which the player spawns.
         private GraphicsDeviceManager graphicsDeviceManager;
-        private List<GameObject> models;
-        private Sun sun;
-        public Camera camera;
-        Landscape landscape;
+        private GameObject[,] terrainGrid;
+        private KeyboardManager keyboardManager;
+        private KeyboardState keyboardState;
+        private MouseManager mouseManager;
+        private MouseState mouseState;
+        public static Camera camera;
+        public static int leftKey = 0, rightKey = 0, upKey = 0, downKey = 0, mouseL, mouseR;
+        public static float mouseX, mouseY;
+        
+        //space for physics
+        public Space space;
+
+
+
         /// <summary>
         /// Initializes a new instance of the <see cref="Project1Game" /> class.
         /// </summary>
@@ -45,70 +66,103 @@ namespace Project1
             // Creates a graphics manager. This is mandatory.
             graphicsDeviceManager = new GraphicsDeviceManager(this);
 
+            // Create the keyboard and mouse managers
+            keyboardManager = new KeyboardManager(this);
+            mouseManager = new MouseManager(this);
+
             // Setup the relative directory to the executable directory
             // for loading contents with the ContentManager
             Content.RootDirectory = "Content";
 
-            // Create the keyboard manager
-
-        }
-
-        protected override void LoadContent()
-        {
-            camera = new Camera(this);
-            models = new List<GameObject>();
-            models.Add(landscape = new Landscape(this));
-            models.Add(sun = new Sun(this));
-            models.Add(new WaterGraphic(this));
-            // Create an input layout from the vertices
-            
-            base.LoadContent();
+            // Generate a new world and serialise it into loadable chunks.
+            FractalTools.GenerateTerrainChunks(tSizeFactor, tRangeFactor, cSizeFactor);
         }
 
         protected override void Initialize()
         {
-            Window.Title = "Lab 2";
+            Window.Title = "Project 1";
+
+            // Set Player spawn zone to be the landscape centre, maximising exploration.
+            lastPlayerZone[0] = lastPlayerZone[1] = (int)Math.Pow(2, tSizeFactor-cSizeFactor)/2;
+
+            // Set camera to begin in the zone of the player. When Player class is implemented, this should be changed.
+            int xPos = lastPlayerZone[0]*chunkWidth+chunkWidth/2,
+                zPos = lastPlayerZone[1]*chunkWidth+chunkWidth/2;
+            camera = new Camera(this, new Vector3(xPos, (float)chunkWidth*3, zPos), Vector3.Normalize(new Vector3(1f, -1f, 1f)));
 
             base.Initialize();
         }
 
+        protected override void LoadContent() {
+
+            // Create an array of terrain chunks for the grid that the player can see.
+            // This grid is centered on the player, and loads/unloads new chunks as the player moves.
+            RebuildGrid();
+            Terrain t = (Terrain)terrainGrid[1, 1];
+            camera.cameraPosition.Y = (float)t.fractal[chunkWidth/2, chunkWidth/2]+10;
+
+            // Create an input layout from the vertices
+            base.LoadContent();
+        }
+
+        private int[] playerZone() {
+            int[] playerZone = {1, 1};
+            //  Until Player class is implemented, just return the zone of the camera.
+            playerZone[0] = (int)camera.cameraPosition.X/chunkWidth;
+            playerZone[1] = (int)camera.cameraPosition.Z/chunkWidth;
+            return playerZone;
+        }
+
+        private void RebuildGrid() {
+            // Create a new terrain grid of chunks, appropriate to where the player is.
+            terrainGrid = new GameObject[loadGridSize, loadGridSize];
+            for (int i = lastPlayerZone[0]-1, gridX = 0; gridX < loadGridSize; i++, gridX++) {
+                for (int j = lastPlayerZone[1]-1, gridZ = 0; gridZ < loadGridSize; j++, gridZ++) {
+                    terrainGrid[gridX, gridZ] = new Terrain(this, cSizeFactor, tRangeFactor, i, j);
+                }
+            }
+        }
+
         protected override void Update(GameTime gameTime)
         {
-
-            camera.Update(gameTime);
-            camera.moveIfBounded(landscape);
-
-            sun.basicEffect.World = Matrix.Scaling(10f) *
-                Matrix.RotationYawPitchRoll((float)gameTime.TotalGameTime.TotalSeconds,
-                (float)gameTime.TotalGameTime.TotalSeconds / 2,
-                (float)gameTime.TotalGameTime.TotalSeconds / 5) *
-                 Matrix.Translation(0f, 350f, 0f)
-                * Matrix.RotationX((float)gameTime.TotalGameTime.TotalSeconds / 20 - 1)
-                * Matrix.Translation(camera.pos); 
-            
-            
-            for (int i = 0; i < models.Count; i++)
-            {
-                models[i].basicEffect.DirectionalLight0.Direction = (Vector3)Vector3.Transform(
-                    -Vector3.Up , Matrix.RotationX((float)gameTime.TotalGameTime.TotalSeconds/20 - 1));
-                models[i].Update(gameTime);
+            // Check where the player is, and replace chunks accordingly. Only replace chunks if player has changed zone.
+            if ((playerZone()[0] != lastPlayerZone[0]) || (playerZone()[1] != lastPlayerZone[1])) {
+                lastPlayerZone = playerZone();
+                RebuildGrid();
             }
-            
 
+            // Update each of the terrain chunks.
+            foreach (GameObject chunk in terrainGrid) { if (chunk != null) chunk.Update(gameTime); };
+
+            // Check if keys are down, and set values so that other classes can be informed
+            keyboardState = keyboardManager.GetState();
+            leftKey  = (keyboardState.IsKeyDown(Keys.Left)  || keyboardState.IsKeyDown(Keys.A)) ? 1 : 0;
+            rightKey = (keyboardState.IsKeyDown(Keys.Right) || keyboardState.IsKeyDown(Keys.D)) ? 1 : 0;
+            upKey    = (keyboardState.IsKeyDown(Keys.Up)    || keyboardState.IsKeyDown(Keys.W)) ? 1 : 0;
+            downKey  = (keyboardState.IsKeyDown(Keys.Down)  || keyboardState.IsKeyDown(Keys.S)) ? 1 : 0;
+
+            // Check mouse state
+            mouseState = mouseManager.GetState();
+            mouseL = mouseState.LeftButton.Down  ? 1 : 0;
+            mouseR = mouseState.RightButton.Down ? 1 : 0;
+            mouseX = mouseState.X;
+            mouseY = mouseState.Y;
+
+            // Update camera
+            camera.Update(gameTime);
+            
             // Handle base.Update
             base.Update(gameTime);
-
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            // Clears the screen with the Color.CornflowerBlue
-            GraphicsDevice.Clear(new Color(0,0,0.2f));
+            // Clears the screen with the background colour
+            GraphicsDevice.Clear(camera.background);
 
-            for (int i = 0; i < models.Count; i++)
-            {
-                models[i].Draw(gameTime);
-            }
+            // Draw each of the terrain chunks.
+            foreach (GameObject chunk in terrainGrid) { if (chunk != null) chunk.Draw(gameTime); };
+
             // Handle base.Draw
             base.Draw(gameTime);
         }
