fdf7df06bf0f4e600b703082e017c6bb59452a3e
Chokolateman <rkarina@student.unimelb.edu.au>
2014-11-06 14:57:19 +1100

    Added screen when a racer game object has reached the goal.

---
+++ Finished.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Project
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Finished : Page
+    {
+        public readonly Project1Game game;
+        private MainPage parent;
+
+        public Finished()
+        {
+
+            this.InitializeComponent();
+        }
+
+        private void cmdMenu_Click(object sender, RoutedEventArgs e)
+        {
+            parent.Children.Remove(this);
+            parent.game.Exit();
+            parent.Children.Add(parent.mainMenu);
+        }
+    }
+}
